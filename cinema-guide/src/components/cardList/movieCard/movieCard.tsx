fec302cc0aff8278f7889141d7cbc861f7caaed9
ovsaltuk <116757654+ovsaltuk@users.noreply.github.com>
2024-08-15 22:27:31 +0300

    feat(ui): plug for movie card background

--- cinema-guide/src/components/cardList/movieCard/movieCard.tsx
+++ cinema-guide/src/components/cardList/movieCard/movieCard.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import { IMovie } from "../../../app/models/IMovie";
 import "./styles.scss";
 import { url } from "inspector";
+import { Icon } from "@iconify/react";
 
 export interface MovieCardProps {
   movie: IMovie;
@@ -14,6 +15,12 @@ export const MovieCard: FC<MovieCardProps> = ({ movie, position }) => {
       className="movie-card"
       style={{ backgroundImage: `url(${movie.posterUrl})` }}
     >
+      {!movie.backdropUrl && (
+        <div className="movie-card__plug">
+          <Icon icon="ph:smiley-sad-fill" />
+          <span>no Image</span>
+        </div>
+      )}
       <span className="movie-card__position">{position}</span>
     </div>
   );
