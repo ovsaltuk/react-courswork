095f27eb40213e9ae1d6d69e0f0db6de2a3495a0
ovsaltuk <116757654+ovsaltuk@users.noreply.github.com>
2024-08-20 23:19:24 +0300

    feat(ui): base footer layout

--- cinema-guide/src/components/footer/footer.tsx
+++ cinema-guide/src/components/footer/footer.tsx
@@ -1,5 +1,16 @@
 import { FC } from "react"
+import "./styles.scss"
+import { Icon } from "@iconify/react"
 
 export const Footer: FC = () => {
-  return(<footer>Footer</footer>)
+  return(<footer className="footer">
+    <div className="container">
+      <div className="footer__copyright">
+      <span>LLC «Мультимедиа Визион»</span>
+      <Icon icon="ic:baseline-copyright" />
+      <span>Все права защищены</span>
+      </div>
+      <div className="footer__socials"></div>
+    </div>
+  </footer>)
 }
